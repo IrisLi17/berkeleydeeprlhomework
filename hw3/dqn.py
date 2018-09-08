3a8382d91042a08961dfe09a80fe5eeaace782bc
Iris Li <461729541@qq.com>
2018-09-08 20:40:33 +0800

    test logsumexp

--- hw3/dqn.py
+++ hw3/dqn.py
@@ -151,7 +151,8 @@ def learn(env,
         y_t = rew_t_ph + gamma * q_tp1_max
     else:
         tau = 0.1
-        v_tp1 = (1.0 - done_mask_ph) * tf.log(tf.reduce_sum(tf.exp(q_tp1 / tau), axis=1))
+        # v_tp1 = (1.0 - done_mask_ph) * tf.log(tf.reduce_sum(tf.exp(q_tp1 / tau), axis=1))
+        v_tp1 = (1.0 - done_mask_ph) * tf.reduce_logsumexp(q_tp1/ tau, axis=1)
         y_t = rew_t_ph + gamma * v_tp1
 
     total_error = tf.losses.huber_loss(y_t, q_t_selected)
